08e89d40c6bf10047e497500bfbe574ed963cd23
Matheus-Estrella <matheus.a.estrella@gmail.com>
2024-04-25 11:32:19 -0300

    Upload

--- Exercicio_Analise_Matriz/Program.cs
+++ Exercicio_Analise_Matriz/Program.cs
@@ -1,61 +1,115 @@
 ﻿
 // MATRIZES
-
-const int size = 5;
-float[,] matriz = new float[size, size];
+const int size = 10;
 int lin, col;
-float[] soma_linhas = new float[size];
-float[] soma_colunas = new float[size];
-float diag_pri=0, diag_sec=0;
 
-for (lin = 0; lin < size; lin++)
+float[,] Gerar_Matriz_Quadrada(int size)
 {
-    for (col = 0; col < size; col++)
-    {
-        matriz[lin, col] = new Random().Next(1, 10);
-        soma_linhas[lin] += matriz[lin, col];
-    }
-}
-for (col = 0; col < size; col++)
-{
+    float[,] matriz = new float[size, size];
     for (lin = 0; lin < size; lin++)
     {
-        soma_colunas[col] += matriz[lin, col];
-        if(lin == col)
+        for (col = 0; col < size; col++)
         {
-            diag_pri += matriz[lin, col];
-        }
-        if(lin != col && lin+col == size)
-        {
-            diag_sec += matriz[lin, col];
+            matriz[lin, col] = new Random().Next(1, 10);
         }
     }
+    return matriz;
 }
-
-Console.WriteLine("\nImprimindo Matriz: ");
-for (lin = 0; lin < size; lin++)
+float[] Somar_Linhas(float[,] matriz, int size)
 {
+    float[] vet = new float[size];
+    for (lin = 0; lin < size; lin++)
+    {
+        for (col = 0; col < size; col++)
+        {
+            vet[lin] += matriz[lin, col];
+        }
+    }
+    return vet;
+}
+float[] Somar_Colunas(float[,] matriz, int size)
+{
+    float[] vet = new float[size];
     for (col = 0; col < size; col++)
     {
-        Console.Write($"{matriz[lin, col]} "); ;
+        for (lin = 0; lin < size; lin++)
+        {
+            vet[col] += matriz[lin, col];
+        }
+    }
+    return vet;
+}
+float Somar_Diagonal_Principal(float[,] matriz, int size)
+{
+    float diag_pri = 0;
+    for (col = 0; col < size; col++)
+    {
+        for (lin = 0; lin < size; lin++)
+        {
+            if (lin == col)
+            {
+                diag_pri += matriz[lin, col];
+            }
+        }
+    }
+    return diag_pri;
+}
+float Somar_Diagonal_Secundaria(float[,] matriz, int size)
+{
+    float diag_sec = 0;
+    for (col = 0; col < size; col++)
+    {
+        for (lin = 0; lin < size; lin++)
+        {
+            if (lin != col && lin + col == size)
+            {
+                diag_sec += matriz[lin, col];
+            }
+        }
+    }
+    return diag_sec;
+}
+void Imprimir_Matriz(float[,] matriz)
+{
+    Console.WriteLine("\nImprimindo Matriz: ");
+    for (int lin = 0; lin < size; lin++)
+    {
+        for (int col = 0; col < size; col++)
+        {
+            Console.Write($"{matriz[lin, col]} "); ;
+        }
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
-Console.WriteLine();
-
-Console.WriteLine("\nImprimindo Soma Linha: ");
-for (lin = 0; lin < size; lin++)
+void Imprimir_Vetor(float[] vet)
 {
-    Console.Write($"{soma_linhas[lin]} ");
-}
-Console.WriteLine();
+    for (int lin = 0; lin < size; lin++)
+    {
+        Console.Write($"{vet[lin]} ");
+    }
+    Console.WriteLine();
 
-Console.WriteLine("\nImprimindo Soma Colunas: ");
-for (lin = 0; lin < size; lin++)
+}
+void Run()
 {
-    Console.Write($"{soma_colunas[lin]} ");
-}
-Console.WriteLine();
+    float[,] matriz = new float[size, size];
+    float[] soma_linhas = new float[size];
+    float[] soma_colunas = new float[size];
+    float diag_pri = 0, diag_sec = 0;
 
-Console.WriteLine($"\nImprimindo Soma da DIagonal Principal: {diag_pri} \n\n");
-Console.WriteLine($"\nImprimindo Soma da DIagonal Secundária: {diag_sec} \n\n");
+    matriz = Gerar_Matriz_Quadrada(size);
+    Imprimir_Matriz(matriz);
+
+    Console.Write("\nImprimindo Soma Linha: ");
+    Imprimir_Vetor(Somar_Linhas(matriz, size));
+
+    Console.Write("\nImprimindo Soma Colunas: ");
+    Imprimir_Vetor(Somar_Colunas(matriz, size));
+
+    Console.WriteLine($"\nImprimindo Soma da Diagonal Principal: {Somar_Diagonal_Principal(matriz, size)} ");
+    Console.WriteLine($"\nImprimindo Soma da Diagonal Secundária: {Somar_Diagonal_Secundaria(matriz, size)} \n");
+
+}
+
+Run();
